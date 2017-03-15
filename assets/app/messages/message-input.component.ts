113c033979c3301007d13977b652c9bca180ee23
Alexander Prater <acprater@gmail.com>
2017-03-15 00:47:44 -0400

    section 46 complete

--- assets/app/messages/message-input.component.ts
+++ assets/app/messages/message-input.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { Message } from './message.model';
+import { MessageService } from './message.service';
+
 @Component({
     selector: 'app-message-input',
-    templateUrl: './message-input.component.html'
+    templateUrl: './message-input.component.html',
+    providers: [MessageService]
 })
 export class MessageInputComponent {
+    constructor(private messageService: MessageService){
+
+    }
+
     onSave(value: string){
-        console.log(value);
+        const message = new Message(value, 'Charlie');
+        this.messageService.addMessage(message);
     }
 }
