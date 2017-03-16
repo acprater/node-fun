92b81333aa123161a2475cf9715deb97bc0e500b
Alexander Prater <acprater@gmail.com>
2017-03-16 02:57:53 -0400

    added Signup Screen
    Section 4 | Lecture 56

--- assets/app/app.routing.ts
+++ assets/app/app.routing.ts
@@ -2,11 +2,12 @@ import { Routes, RouterModule } from "@angular/router";
 
 import { AuthenticationComponent } from './auth/authentication.component';
 import { MessagesComponent } from './messages/messages-component';
+import { AUTH_ROUTES } from "./auth/auth.routes";
 
 const APP_ROUTES: Routes = [
     { path: '', redirectTo: '/messages', pathMatch: 'full' }, // pathMatch full forces the path check to ensure there is nothing in url
     { path: 'messages', component: MessagesComponent },
-    { path: 'auth', component: AuthenticationComponent }
+    { path: 'auth', component: AuthenticationComponent, children: AUTH_ROUTES }
 ];
 
 export const routing = RouterModule.forRoot(APP_ROUTES);
