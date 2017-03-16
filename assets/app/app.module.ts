b2ee3476903e0ded2cfcc901fec8a87d945b63b4
Alexander Prater <acprater@gmail.com>
2017-03-16 00:36:10 -0400

    added header.component navigation on index.
    section 4 - lecture 50

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -2,17 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from "@angular/forms";
 
+import { HeaderComponent } from './header.component';
 import { AppComponent } from "./app.component";
+import { AuthenticationComponent } from './auth/authentication.component';
 import { MessageComponent } from "./messages/message.component";
 import { MessageListComponent } from './messages/message-list.component';
 import { MessageInputComponent } from './messages/message-input.component';
+import { MessagesComponent } from './messages/messages-component';
 
 @NgModule({
     declarations: [
         AppComponent,
         MessageComponent,
         MessageListComponent,
-        MessageInputComponent
+        MessageInputComponent,
+        MessagesComponent,
+        AuthenticationComponent,
+        HeaderComponent
     ],
     imports: [BrowserModule, FormsModule],
     bootstrap: [AppComponent]
