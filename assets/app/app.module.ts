50e472b97651daf82d96c149b059a545b0070186
Alexander Prater <acprater@gmail.com>
2017-03-18 13:42:25 -0400

    setup lazy loading for auth module

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -1,15 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { ReactiveFormsModule } from "@angular/forms";
 import { HttpModule } from '@angular/http';
 
 import { routing } from './app.routing';
 import { AppComponent } from "./app.component";
 import { AuthenticationComponent } from './auth/authentication.component';
 import { HeaderComponent } from './header.component';
-import { LogoutComponent } from './auth/logout.component';
-import { SigninComponent } from './auth/signin.component';
-import { SignupComponent } from './auth/signup.component';
 import { AuthService } from "./auth/auth.service";
 import { ErrorComponent } from "./errors/error.component";
 import { ErrorService } from "./errors/error.service";
@@ -21,15 +17,11 @@ import { MessageModule } from './messages/message.module';
         AppComponent,
         AuthenticationComponent,
         HeaderComponent,
-        LogoutComponent,
-        SigninComponent,
-        SignupComponent,
         ErrorComponent
     ],
     imports: [
-        BrowserModule, 
-        routing, 
-        ReactiveFormsModule,
+        BrowserModule,
+        routing,
         HttpModule,
         MessageModule
     ],
--- assets/app/app.routing.ts
+++ assets/app/app.routing.ts
@@ -2,12 +2,11 @@ import { Routes, RouterModule } from "@angular/router";
 
 import { AuthenticationComponent } from './auth/authentication.component';
 import { MessagesComponent } from './messages/messages-component';
-import { AUTH_ROUTES } from "./auth/auth.routes";
 
 const APP_ROUTES: Routes = [
     { path: '', redirectTo: '/messages', pathMatch: 'full' }, // pathMatch full forces the path check to ensure there is nothing in url
     { path: 'messages', component: MessagesComponent },
-    { path: 'auth', component: AuthenticationComponent, children: AUTH_ROUTES }
+    { path: 'auth', component: AuthenticationComponent, loadChildren: './auth/auth.module#AuthModule' }
 ];
 
 export const routing = RouterModule.forRoot(APP_ROUTES);
