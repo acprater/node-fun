e410191cf3e7d3c50c690edb0378992579c229ac
Alexander Prater <acprater@gmail.com>
2017-03-17 23:14:33 -0400

    signin changes complete.  uses mongo to validate user is correct.

--- assets/app/auth/signin.component.ts
+++ assets/app/auth/signin.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from "@angular/forms";
+import { Router } from '@angular/router';
+
+import { User } from "./user.model";
+import { AuthService } from "./auth.service";
 
 @Component({
     selector: 'app-signin',
@@ -7,9 +11,20 @@ import { FormGroup, FormControl, Validators } from "@angular/forms";
 })
 export class SigninComponent {
     myForm: FormGroup;
+    
+    constructor(private authService: AuthService, private router: Router) {}
 
     onSubmit() {
-        console.log(this.myForm);
+        const user = new User(this.myForm.value.email, this.myForm.value.password);
+        this.authService.signin(user)
+            .subscribe(
+                data => {
+                    localStorage.setItem('token', data.token);
+                    localStorage.setItem('userId', data.token);
+                    this.router.navigateByUrl('/');
+                },
+                error => console.error(error)
+            );
         this.myForm.reset();
     }
 
